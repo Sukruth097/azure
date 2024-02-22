588da9719a3d98dd90fdfbdb3c027beef561dd65
Sukruth A V (SONATA SOFTWARE NORTH AMERICA) <v-sukruthav@microsoft.com>
2024-02-22 15:40:55 +0530

    intial commit

---
+++ init_setup.sh
@@ -0,0 +1,22 @@
+echo [$(date)]: "START"
+echo [$(date)]: "creating environment"
+conda create -p azure python=3.8 -y
+echo [$(date)]: "activate environment"
+source activate venv/
+
+echo [$(date)]: "install requirements"
+pip install -r requirements.txt
+# echo [$(date)]: "export conda environment"
+# conda env export > conda.yaml
+echo "# ${PWD}" > README.md
+echo [$(date)]: "first commit"
+# git init
+# git remote add origin https://github.com/Sukruth097/azure.git
+# git branch -M main #{branch name}
+# git add .
+# git commit -m "first commit"
+# git push -u origin main
+echo [$(date)]: "END"
+
+# to remove everything -
+# rm -rf venv/ .gitignore conda.yaml README.md .git/
