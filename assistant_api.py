d6f7f3531b2896d391a26bfa67cb7480e55052df
Srikrupa <srikrupa738@gmail.com>
2024-03-07 21:11:47 +0530

    Assistant_API

---
+++ assistant_api.py
@@ -0,0 +1,36 @@
+import os
+import json
+from openai import AzureOpenAI
+    
+client = AzureOpenAI(
+    # api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
+    api_key="d6005d5de77c486d89c9addf6abb6d3c",
+    azure_endpoint = "https://aoaipre11.openai.azure.com/",
+    api_version="2024-02-15-preview"
+    # azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+    )
+
+# Create an assistant
+assistant = client.beta.assistants.create(
+    name="Data Visualization",
+    instructions=f"You are a helpful AI assistant who makes interesting visualizations based on data." 
+    f"You have access to a sandboxed environment for writing and testing code."
+    f"When you are asked to create a visualization you should follow these steps:"
+    f"1. Write the code."
+    f"2. Anytime you write new code display a preview of the code to show your work."
+    f"3. Run the code to confirm that it runs."
+    f"4. If the code is successful display the visualization."
+    f"5. If the code is unsuccessful display the error message and try to revise the code and rerun going through the steps from above again.",
+    tools=[{"type": "code_interpreter"}],
+    model="rpmcheck" #You must replace this value with the deployment name for your model.
+    
+)
+
+
+
+print(assistant.model_dump_json(indent=2)[0])
+# result = assistant.model_dump_json(indent=2)
+# file= json.loads(result)
+# print(file["name"])
+
+
